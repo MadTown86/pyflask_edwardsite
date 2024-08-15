e56de425631d3a7d9199e0d92c08f1dc52e2ff4e
MadTown86 <grover.donlon@gmail.com>
2024-08-15 15:15:43 -0500

    works finally on visions.fit domain.  Now I have to add more complex routes plus db communication in python/flask vs. node/js/express.

--- app.py
+++ app.py
@@ -1,56 +1,75 @@
-from flask import Flask, render_template
+import logging
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
+from flask import Flask, render_template, \
+request, redirect, url_for, session
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
 import time as tt
 
+# Load Environment Variables
+load_dotenv()
+
+# Create Flask App
 app = Flask(__name__)
+
+# Logging
+handler = RotatingFileHandler("C:\REPOSITORIES\\PYTHON\\pyflask_edwardsite\\flask_log", maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
 
 year = tt.strftime("%Y")
 
 # Basic Routes
 #region Basic Routes
 @app.route("/")
-def index_page(year=year):
-    return render_template("/pages/index.jinja", year=year)
+def index_page():
+    return render_template("pages/index.jinja", year=year)
+
 
 @app.route("/vision")
-def vision_page(year=year):
+def vision_page():
     return render_template("/pages/vision.jinja", year=year)
 
+
 @app.route("/train")
-def train_page(year=year):
+def train_page():
     return render_template("/pages/train.jinja", year=year)
 
+
 @app.route("/reset_request")
-def reset_request_page(year=year):
+def reset_request_page():
     return render_template("/pages/reset_request.jinja", year=year)
 
 @app.route("/contact")
-def contact_page(year=year):
+def contact_page():
     return render_template("/pages/contact.jinja", year=year)
 
-@app.route("/register")
-def register_page(year=year):
+
+@app.route("/register", methods=['GET', 'POST'])
+def register_page():
+    if request.method == 'POST':
     return render_template("/pages/register.jinja", year=year)
 
+
 @app.route("/privacy_policy")
-def privacy_policy_page(year=year):
+def privacy_policy_page():
     return render_template("/pages/privacy_policy.jinja", year=year)
 
+
 @app.route("/login")
-def login_page(year=year):
+def login_page():
     return render_template("/pages/login.jinja", year=year)
 
+
 @app.route("/food")
-def food_page(year=year):
+def food_page():
     return render_template("/pages/food.jinja", year=year)
+
 #endregion
 
-
-
-
-# if __name__ == "__main__":
-#     print(year)
-#     app.run(
-#         host='localhost',
-#         port=5000,
-#         debug=True
-#     )
+# Run App
+if __name__ == "__main__":
+    app.run(debug=True)
